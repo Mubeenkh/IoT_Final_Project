922567e1724018bb3e59ee4f0c8aa900b42422bf
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2023-10-01 15:54:37 -0400

    fixed the host IP at the end

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def update_button(n_clicks):
 
 # Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-    # app.run(debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)

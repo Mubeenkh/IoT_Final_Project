a3e32eac40e2a2c0142e2f76a98f52b2c070fc51
Mubeenkh <89649704+Mubeenkh@users.noreply.github.com>
2023-10-19 21:24:12 -0400

    moved some html around

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 # import RPi.GPIO as GPIO
 from dash import Dash, html, Input, Output, State, callback, dcc
 import dash_daq as daq
-# -----------------------------------------------
+
+# SMTP client session object that can be used to send mail to any internet machine with an SMTP
 import smtplib
 import imaplib
 import email
@@ -13,16 +14,14 @@ from time import sleep
 import secrets
 import string
 
-
-
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from itertools import chain
 
 
-
-subject = "The current temperature is ***. Would you like to turn on the fan?"
-body = ""
+# -----------------------------------------------
+subject = ""
+body = "The current temperature is ***. Would you like to turn on the fan?"
 
 sender = "python01100922@gmail.com"
 password = "txlzudjyidtoxtyj"
@@ -32,6 +31,8 @@ recipients = "extramuffin0922@gmail.com"
 token_length = 16
 
 
+# -----------------------------------------------
+# RPi components :
 # GPIO.setwarnings(False)
 # GPIO.setmode(GPIO.BCM)
 
@@ -61,117 +62,95 @@ style_img_light_on = {
     'filter': 'drop-shadow(1px 1px 20px rgba(255, 255, 0, 1))',
 }
 
-
 # Fan images and CSS
 fan_off = 'assets/images/fan.png'
 fan_on = 'assets/images/fan.gif'
-style_img_fan = {
-    'height': '150px',
-    'width':'150px',
-}
 
+# content for later
+profile_content =[
+    html.Img( src='assets/images/user.png',  style={'height': '100px', 'width':'100px',} ),
+    html.Div(style={'text-align':'left'},children=[
+        html.P('Username:'),
+        html.P('Favorites:'),
+        html.P('Temperature:'),
+        html.P('Humidity:'),
+        html.P('Light intensity:'),   
+    ])
+]
+
+# Phase 2 content
+fan_content = [
+
+    html.Div(id='therm-hum-display',children=[
+        html.Div(
+            daq.Thermometer(             
+                showCurrentValue=True, label='Temperature (°C)',
+
+                # TODO: change the value with current Temperature °C
+                height=120,min=-10,max=40,units="°C",
+                value=20, 
+            )
+        ),
+        html.Div(
+            daq.Gauge(
+                color={"gradient":True,"ranges":{"green":[0,60],"yellow":[60,80],"red":[80,100]}},
+                showCurrentValue=True, label='Humidity (%)',
+
+                # TODO: change the value with current Humidity %
+                size=150,min=0,max=100,units="%",
+                value=20
+            ),
+        ),
+
+    ]),
+    html.Div(children=[
+
+        html.Div(children=[
+
+            html.Img( src='assets/images/fan.png', className="feature-img", id='fan-img' ),
+
+        ]),
+        
+        html.Div([
+            html.Button( 'Fan On', className="button-style", id='fan-switch',  n_clicks=0 )
+        ]) 
+
+    ]),
+
+];
+
+# Phase 1 content
+light_content =[
+
+    html.Div(id='feature-container',children=[
+        html.Div([
+            html.Img( src=img_light_off, className="feature-img", id='light-img' )
+        ]),
+        # html.Div([
+        #     html.Button( 'Turn On', className="button-style", id='light-switch', n_clicks=0 )
+        # ])
+        daq.BooleanSwitch( on=False, id='light-switch', className='dark-theme-control' ),
+    ]),
+    
+    html.Div(id='feature-container',children=[
+        html.Div([ 'Phone thing' ]),
+    ])
+        
+];
 
 # App layout
 app.layout = html.Div( id='layout',style={},
     children=[
-
+        html.H2(style={},children=['IoT Project']),
         html.Div(id='container', children=[
-            html.H2('IoT Project'),
-
+            
             html.Div(id='column', children=[
-                html.Div(id='feature-container', children=[
-                    html.Div([
-                        html.Img(
-                            src=img_light_off,
-                            className="feature-img",
-                            id='light-img',
-                            style={}
-                        )
-                    ]),
-                    html.Div([
-                        html.Button(
-                            'Turn On', 
-                            className="button-style", 
-                            id='light-switch', 
-                            n_clicks=0,
-                            style={}
-                        )
-                    ])
-                        
-                ]),
 
-                html.Div(id='feature-container',children=[
-                    html.Div(children=[
+                html.Div(style={'border':'1px solid black','backgroundColor':'grey', 'padding':'20px'},children=profile_content),
 
-                        html.Div(
-                            # daq.Gauge(
-                            #     showCurrentValue=True,
-                            #     units="°C",
-                            #     # TODO: change the value with current temperature
-                            #     value=20,
-                            #     min=-50,
-                            #     max=50,
-                            #     size=150
-                            # ),
-                            daq.Thermometer(
-                                min=-10,
-                                max=40,
-                                value=20,
-                                showCurrentValue=True,
-                                units="C",
-                                label='Temperature',
-                                style={'font-size':'100px'}
-                            )
+                html.Div(id='fan-container',children=fan_content),
+                html.Div(id='light-container', children=light_content),
 
-                        ),
-                        html.Div(
-                            daq.Gauge(
-                                color={
-                                    "gradient":True,
-                                    "ranges":{
-                                        "green":[0,60],
-                                        "yellow":[60,80],
-                                        "red":[80,100]
-                                    }
-                                },
-                                label='Humidity',
-                                showCurrentValue=True,
-                                units="%",
-                                # TODO: change the value with current Humidity %
-                                value=20,
-                                min=0,
-                                max=100,
-                                size=150
-                            ),
-                        ),
-
-                    ]),
-
-                ]),
-                html.Div(id='feature-container',children=[
-
-                    html.Div(children=[
-
-                        html.Img(
-                            src='assets/images/fan.png',
-                            className="feature-img", 
-                            id='fan-img',
-                            style=style_img_fan
-                        ),
-
-                    ]),
-                    
-                    html.Div([
-                        html.Button(
-                            'Fan On',
-                            className="button-style", 
-                            id='fan-switch', 
-                            n_clicks=0,
-                            style={}
-                        )
-                    ])
-                    
-                ]),
             ])
         ]),
 
@@ -179,25 +158,36 @@ app.layout = html.Div( id='layout',style={},
 )
 
 
+# @app.callback(
+#     [Output('light-switch', 'children'),
+#      Output('light-switch', 'title'),
+#      Output('light-img', 'src'),
+#      Output('light-img', 'style'),],
+#     Input('light-switch', 'n_clicks')
+# )
+# def update_button(n_clicks):
+    # bool_disabled = n_clicks % 2
+    # if bool_disabled:
+    #     # GPIO.output(LED,1)
+    #     return 'Turn Off', 'Too bright', img_light_on, style_img_light_on
+    # else: 
+    #     # GPIO.output(LED,0)
+    #     return 'Turn On','Too dark', img_light_off, style_img
 
 @app.callback(
-    [Output('light-switch', 'children'),
-    #  Output('light-switch', 'style'),
-     Output('light-switch', 'title'),
-     Output('light-img', 'src'),
+    [Output('light-img', 'src'),
      Output('light-img', 'style'),],
-    Input('light-switch', 'n_clicks')
+    Input('light-switch', 'on')
 )
-
-def update_button(n_clicks):
-    bool_disabled = n_clicks % 2
-    if bool_disabled:
+def update_button(on):
+    print(on)
+    if on == True:
         # GPIO.output(LED,1)
-        return 'Turn Off', 'Too bright', img_light_on, style_img_light_on
+        return img_light_on, style_img_light_on
     else: 
         # GPIO.output(LED,0)
-        return 'Turn On','Too dark', img_light_off, style_img
-
+        return img_light_off, style_img
+    
 
 @app.callback(
     [Output('fan-switch','children'),
@@ -208,14 +198,6 @@ def update_button(n_clicks):
 def control_fan(n_clicks):
     # print("dfd");
     bool_disabled = n_clicks % 2
-    """ TODO find a way to resolve that
-    if bool_disabled:
-        GPIO.output(LED,1)
-        return 'Fan is On', fan_on
-    else: 
-        GPIO.output(LED,0)
-        return 'Fan is off', fan_off
-    """
     
     if bool_disabled:
         reply = receiveRecentEmail()
@@ -229,8 +211,6 @@ def control_fan(n_clicks):
         n_clicks == 0
         return 'Fan is off', fan_off
     
-
-
 
 
 # GET the most RECENT email from recipients
@@ -255,16 +235,16 @@ def receiveRecentEmail():
             message = email.message_from_string(data[0][1].decode("utf-8"))
 
             from_ = email.utils.parseaddr(message.get('From'))[1] #to only get the email address
-            to_ = message.get('To')
-            date_ = message.get('Date')
+            # to_ = message.get('To')
+            # date_ = message.get('Date')
             subject_ = message.get('Subject')
 
             # if subject_ == "Re: " + subject: 
             print('#-----------------------------------------#')
             print(f"From: {from_}")  #to only get the email address
-            print(f"To: {to_}")
-            print(f"Date: {date_}")
-            print(f"Subject: {subject_}")
+            # print(f"To: {to_}")
+            # print(f"Date: {date_}")
+            # print(f"Subject: {subject_}")
             print("Content:")
             # print (f'{subject} {unique_token}')
             for part in message.walk():
@@ -312,7 +292,7 @@ def send_email(subject, body, sender, recipients, password, unique_token):
     msg['From'] = sender
     msg['To'] = recipients
     # msg.attach(MIMEText(body, 'plain'))
-    msg.attach(MIMEText(subject))
+    msg.attach(MIMEText(body))
 
     print("Connecting to server..")
     smtp_server =  smtplib.SMTP_SSL('smtp.gmail.com', 465)
